d7dbf0434322452516381237da4e1f394be5c473
Harish Sandiparthi <h.sandiparthi1@gmail.com>
2022-11-11 10:51:54 +0530

    refactor: Refactored controller class to use service class

--- src/main/java/com/harish/controller/BowlingGameController.java
+++ src/main/java/com/harish/controller/BowlingGameController.java
@@ -1,5 +1,6 @@
 package com.harish.controller;
 
+import com.harish.service.BowlingGameService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BowlingGameController {
 
+    private BowlingGameService bowlingGameService;
+
+    public BowlingGameController(BowlingGameService bowlingGameService) {
+        this.bowlingGameService = bowlingGameService;
+    }
+
     @GetMapping(path = "/getScore")
     public int gameHandler(@RequestParam int[] game) {
-        return 0;
+        return bowlingGameService.getGameScore();
     }
 }
--- src/test/java/com/harish/controller/BowlingGameControllerTests.java
+++ src/test/java/com/harish/controller/BowlingGameControllerTests.java
@@ -1,12 +1,15 @@
 package com.harish.controller;
 
+import com.harish.service.BowlingGameService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -17,9 +20,13 @@ public class BowlingGameControllerTests {
     @Autowired
     private MockMvc mvc;
 
+    @MockBean
+    private BowlingGameService bowlingGameService;
+
     @Test
     public void gameHandlerApiFoundTest() throws Exception {
 
+        when(bowlingGameService.getGameScore()).thenReturn(0);
         mvc.perform(get("/getScore?game=0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0"))
                 .andExpect(status().isOk());
     }
