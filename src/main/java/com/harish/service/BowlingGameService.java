98862217a94904fbeb21eafe61eb88026269e81f
Harish Sandiparthi <h.sandiparthi1@gmail.com>
2022-11-11 20:25:52 +0530

    refactor: Extract score calaculation for better code readability

--- src/main/java/com/harish/service/BowlingGameService.java
+++ src/main/java/com/harish/service/BowlingGameService.java
@@ -6,24 +6,39 @@ import org.springframework.stereotype.Service;
 public class BowlingGameService {
 
     private int[] rolls;
+    private int gameScore;
 
     public int getGameScore(int[] eachGame) {
         rolls = eachGame;
-        int score = 0;
-        int roll = 0;
-        for(int frame = 0; frame < 10; frame++) {
+        calculateScore();
+        return gameScore;
+    }
+
+    private void calculateScore() {
+
+        for(int frame = 0, roll=0; frame < 10; frame++, roll++) {
             if(isStrike(rolls[roll])) {
-                score += 10 + rolls[roll+1] + rolls[roll+2];
+                scoreStrike(roll);
+            }else if(isSpare(roll)) {
+                scoreSpare(roll);
                 roll++;
-            } else if(isSpare(roll)) {
-                score += 10 + rolls[roll+2];
-                roll += 2;
-            } else {
-                score += rolls[roll] + rolls[roll+1];
-                roll += 2;
+            }else {
+                scoreFrame(roll);
+                roll++;
             }
         }
-        return score;
+    }
+
+    private void scoreFrame(int roll) {
+        gameScore += rolls[roll] + rolls[roll+1];
+    }
+
+    private void scoreSpare(int roll) {
+        gameScore += 10 + rolls[roll+2];
+    }
+
+    private void scoreStrike(int roll) {
+        gameScore += 10 + rolls[roll +1] + rolls[roll +2];
     }
 
     private boolean isStrike(int roll) {
